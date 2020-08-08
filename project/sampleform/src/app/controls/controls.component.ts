95e766af1681b25cc54eafa95b00e5fc9998b285
yosshi-4989 <archer0729@icloud.com>
2020-08-08 17:39:57 +0000

    FormBuilderにおけるバリデーションの入れ方

--- project/sampleform/src/app/controls/controls.component.ts
+++ project/sampleform/src/app/controls/controls.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, FormArray } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-controls',
@@ -16,8 +16,8 @@ export class ControlsComponent implements OnInit {
   // とりあえずフォームの管理が簡単になるようなものという認識で行く
   constructor(private fb: FormBuilder) {
     this.coffeeForm = this.fb.group({
-      name: "ブレンド",
-      taste: "バランスのよい口当たり",
+      name: ["ブレンド", Validators.required], // 今回は確定がないので意味ないが、リストにして渡すことでバリデーションを指定できる
+      taste: ["バランスのよい口当たり", [Validators.required, Validators.maxLength(10)]], // 複数のバリデーションは前回と同じようにリストにする
       hotcold: this.hotcoldsel[0],
       adds: this.fb.array([]),
       nut: this.nutsel[0],
@@ -26,9 +26,6 @@ export class ControlsComponent implements OnInit {
 
   get name() { return this.coffeeForm.get("name"); }
   get taste() { return this.coffeeForm.get("taste"); }
-  get hotcold() { return this.coffeeForm.get("hotcold"); }
-  get adds() { return this.coffeeForm.get("adds"); }
-  get nut() { return this.coffeeForm.get("nut"); }
 
   onCheckChanged(item: string, isChecked: boolean) {
     let formArray = <FormArray>this.coffeeForm.controls.adds;
