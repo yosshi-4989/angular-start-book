f5ba627a03b05c8fb281cc9a216f22be49e071f0
yosshi-4989 <archer0729@icloud.com>
2020-08-09 02:19:24 +0000

    一覧ページの追加

--- project/cookbook/src/app/app.module.ts
+++ project/cookbook/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule
